b983a56eefbf70a3f22e4a7aa17e25c792db6dab
code-house10 <107010368+code-house10@users.noreply.github.com>
2026-06-09 22:25:20 +0300

    malek5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jungle-movie-mymemory-template-examples-v4-merged-good-examples-delete';
+const CACHE_NAME = 'jungle-movie-puter-ai-v1';
 const APP_SHELL = [
   './',
   './index.html',
